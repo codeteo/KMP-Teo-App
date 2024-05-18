c01e48e944f56a7fce9962942e333db1a272c37a
Theophilos Ioakeimidis <ramones.teo@gmail.com>
2024-05-18 20:25:44 +0300

    Updates ktor negotiation plugin

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/network/NetworkClient.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/network/NetworkClient.kt
@@ -5,17 +5,19 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 object NetworkClient {
 
     fun provideHttpClient(): HttpClient {
-        val json = Json { ignoreUnknownKeys = true }
         return HttpClient {
             install(ContentNegotiation) {
-                json(json, contentType = ContentType.Any)
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
             }
             install(Logging) {
                 logger = Logger.DEFAULT
